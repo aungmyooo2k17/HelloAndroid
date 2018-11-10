aec5bc3a55fe3c218732738f3c2cbf7168c6cd31
Aung Myo Oo <dev.aungmyooo2015@gmail.com>
2018-11-10 20:08:13 +0630

    2nd

---
+++ app/src/main/java/me/aungmyooo/helloandroid/LInearLayoutActivity.java
@@ -0,0 +1,13 @@
+package me.aungmyooo.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LInearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
